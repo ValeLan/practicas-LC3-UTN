4f1fb9b90ea8da5e381709a6590af8bde79fc84f
ValeLan <vale.lanf@gmail.com>
2024-08-29 20:30:34 -0300

    finalizada la practica 2

---
+++ practica-2/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Table from './components/table/Table'
+
+const netIncomes = [{brand: "McDonalds", income: 1291283}, {brand: "Burger King", income: 1927361}, {brand: "KFC", income: 1098463}];
+
+function App() {
+
+  return (
+    <>
+      <Table netIncomes={netIncomes}/>
+    </>
+  )
+}
+
+export default App
